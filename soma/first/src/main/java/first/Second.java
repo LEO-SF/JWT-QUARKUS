bce38d72a1d9b8f363c003582a12a42dbc436b68
LEO-SF <71852506+LEO-SF@users.noreply.github.com>
2023-09-28 14:13:45 -0300

    Resolução User
    Resolução da parte do exercício User, onde é necessário gerar um token para o usuário ter acesso aos serviços

--- soma/first/src/main/java/first/Second.java
+++ soma/first/src/main/java/first/Second.java
@@ -1,33 +1,27 @@
 package first;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import io.quarkus.oidc.token.propagation.AccessToken;
+import io.quarkus.vertx.web.Param;
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.FormParam;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/first")
-public class First {
 
-    @Inject
-    @RestClient
-    Second retorno;
+@RegisterRestClient(baseUri = "https://localhost:8445")
+@AccessToken
+public interface Second {
 
     @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/second")
     @Produces(MediaType.TEXT_PLAIN)
     @RolesAllowed("User")
-    public int sum(
-        @FormParam("a") int a,
-        @FormParam("b") int b) {     
-        return retorno.getSoma(a,b);
-    }
+    int getSoma(
+        @FormParam("a") int a, @FormParam("b") int b);
+        
+
 }
